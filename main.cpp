5bed4c4f1f309faabd1cdc48e395435eac802a74
garrett1001 <31227173+garrett1001@users.noreply.github.com>
2018-04-17 21:35:11 -0700

    Update main.cpp
    complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void rearrangeNumbers(int &red, int &green, int &blue);
+
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -9,7 +11,7 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
+  rearrangeNumbers(red, green, blue);
 
 
 
@@ -18,4 +20,56 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+
+void rearrangeNumbers(int &red, int &green, int &blue)
+{ 
+  int one;
+  int two;
+  
+  if(red>green && red>blue) 
+  {
+      if(green<blue)
+      {
+       one = green;
+       green = blue;
+       blue = one;
+      }
+  }
+  
+  else if(green>red && green>blue)
+  {
+      one = red;
+      
+      if(red>blue)
+      {
+          red = green;
+          green = red;
+      }
+      
+      else
+      {
+          red = green;
+          green = blue;
+          blue = one;
+      }
+  }
+  
+  else if(blue>red && blue>green)
+  {
+      one = red;
+      
+      if(red>green)
+      {
+          red = blue;
+          blue = green;
+          green = one;
+      }
+      
+      else
+      {
+          red = blue;
+          blue = one;
+      }
+  }
 }
